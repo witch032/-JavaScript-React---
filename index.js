acb37fa73f8e18ee3fa9fa4b1f9e5f96cc5a3619
witch032 <witch032@gmail.com>
2023-03-22 17:35:09 +0200

    Практика, ч.1. Начинаем создавать приложение
    Практика, ч.1. Начинаем создавать приложение

--- index.js
+++ index.js
@@ -1,47 +1,40 @@
-console.log('arr' + '- object');
-console.log(4 + '5');
+/* Задание на урок:
+1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
+'Сколько фильмов вы уже посмотрели?'
+2) Создать объект personalMovieDB и в него поместить такие свойства:
+    - count - сюда передается ответ на первый вопрос
+    - movies - в это свойство поместить пустой объект
+    - actors - тоже поместить пустой объект
+    - genres - сюда поместить пустой массив
+    - privat - в это свойство поместить boolean(логическое) значение false
+3) Задайте пользователю по два раза вопросы:
+    - 'Один из последних просмотренных фильмов?'
+    - 'На сколько оцените его?'
+Ответы стоит поместить в отдельные переменные
+Записать ответы в объект movies в формате: 
+    movies: {
+        'logan': '8.1'
+    }
+Проверить, чтобы все работало без ошибок в консоли */
 
-let incr = 10,
-    decr = 10;
+let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?',' '); 
 
-// incr++;
-// decr--;
+console.log(numberOfFilms);
 
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {} ,
+    actors: {} ,
+    genres: [] ,
+    privat: false,
+}
 
-// console.log(incr);
-// console.log(decr);
+console.log(personalMovieDB);
 
-// console.log(incr++);
-// console.log(decr--);
+let a = prompt('Один из последних просмотренных фильмов?',' '),
+    b = prompt('На сколько оцените его?',' '),
+    c = prompt('Один из последних просмотренных фильмов?',' '),
+    d = prompt('На сколько оцените его?',' ');  
 
-console.log(++incr);
-console.log(--decr);
-
-
-console.log(5%2);
-
-
-console.log(2*4 == 8 );
-
-console.log(2*4 === '8' );
-
-
-// const isCheced = true,
-//       isClose = true;
-
-// console.log(isCheced && isClose);
-
-// const isCheced = true,
-//       isClose = false;
-
-// console.log(isCheced && isClose);
-
-// const isCheced = true,
-//       isClose = false;
-
-// console.log(isCheced || isClose);
-
-const isCheced = false,
-      isClose = false;
-
-console.log(isCheced || !isClose);
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
