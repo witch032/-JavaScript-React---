85cc95883dd7282c40335ca823b4c073c27d3499
witch032 <witch032@gmail.com>
2023-03-22 16:06:25 +0200

    new changes

---
+++ index.js
@@ -0,0 +1,47 @@
+console.log('arr' + '- object');
+console.log(4 + '5');
+
+let incr = 10,
+    decr = 10;
+
+// incr++;
+// decr--;
+
+
+// console.log(incr);
+// console.log(decr);
+
+// console.log(incr++);
+// console.log(decr--);
+
+console.log(++incr);
+console.log(--decr);
+
+
+console.log(5%2);
+
+
+console.log(2*4 == 8 );
+
+console.log(2*4 === '8' );
+
+
+// const isCheced = true,
+//       isClose = true;
+
+// console.log(isCheced && isClose);
+
+// const isCheced = true,
+//       isClose = false;
+
+// console.log(isCheced && isClose);
+
+// const isCheced = true,
+//       isClose = false;
+
+// console.log(isCheced || isClose);
+
+const isCheced = false,
+      isClose = false;
+
+console.log(isCheced || !isClose);
